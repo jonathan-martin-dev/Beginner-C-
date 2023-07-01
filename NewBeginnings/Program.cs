b3e3669e7033b8baaaf66a5cbad968487c36e70b
Jonathan Martinez <57384298+jonathan-martin-dev@users.noreply.github.com>
2023-07-01 14:44:18 -0700

    Working with int datatype.

---
+++ NewBeginnings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Declaring a variable
+        int num1;
+        //Assign variable
+        num1 = 13;
+        //declaring and initializing variable
+        int num2 = 23;
+        int sum = num1 + num2;
+
+        Console.WriteLine(sum);
+        Console.Read();
+    }
+}
