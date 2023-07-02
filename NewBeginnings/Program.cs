d32612f4516096085260b6baf5ebb060e8ec7835
Jonathan Martinez <57384298+jonathan-martin-dev@users.noreply.github.com>
2023-07-02 10:39:09 -0700

    Using string methods

--- NewBeginnings/Program.cs
+++ NewBeginnings/Program.cs
@@ -5,19 +5,28 @@ internal class Program
     private static void Main(string[] args)
     {
         //Declaring a variable
-        int num1;
+        // int num1;
         //Assign variable
-        num1 = 13;
+        //num1 = 13;
         //declaring and initializing variable
-        int num2 = 23;
-        int sum = num1 + num2;
+        //int num2 = 23;
+        //int sum = num1 + num2;
 
-        Console.WriteLine(sum);
+        /* Console.WriteLine(sum);
 
         float f1 = 3.145678f;
         float f2 = 2.5423f;
 
         float fDiv = f1 / f2;
-        Console.WriteLine(fDiv);
+        Console.WriteLine(fDiv);*/
+
+        string myname = "Jon";
+        string message = "My name is " + myname;
+
+        string capsMessage = message.ToUpper();
+
+        Console.WriteLine(capsMessage);
+        Console.Read();
+
     }
 }
